8595740662fc574ce89d636c7188554e5b8a2bb1
MSUIT1908 <166536637+MSUIT1908@users.noreply.github.com>
2025-05-07 11:51:31 -0400

    Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -6075,10 +6075,11 @@ lifesize)
     ;;
 lightburn)
     name="LightBurn"
-    type="dmg"
-    downloadURL="$(downloadURLFromGit LightBurnSoftware deployment)"
-    appNewVersion="$(versionFromGit LightBurnSoftware deployment)"
-    expectedTeamID="UWZQ3LL82C"
+    type="dmg"   
+    appNewVersion=$(curl -s https://release.lightburnsoftware.com/LightBurn/Release/ | grep -o 'LightBurn-v[0-9]\+\.[0-9]\+\.[0-9]\+' | sed 's/LightBurn-v//g' | sort -V | tail -1)
+   	downloadURL="https://files.release.lightburnsoftware.com/LightBurn/Release/LightBurn-v$appNewVersion/LightBurn.V$appNewVersion.dmg"
+ 	expectedTeamID="UWZQ3LL82C"
+    versionKey="CFBundleShortVersionString"
     ;;
 linear)
     name="Linear"
